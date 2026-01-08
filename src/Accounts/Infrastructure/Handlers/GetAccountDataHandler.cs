fc9fe230850fc59f843c3c99e8ca955d2c6b7eee
Miha Jakovac <miha@mjc.si>
2026-01-08 08:48:35 +0100

    Updated code for new release cli 1.16.0 and host 1.6.0

--- src/Accounts/Infrastructure/Handlers/GetAccountDataHandler.cs
+++ src/Accounts/Infrastructure/Handlers/GetAccountDataHandler.cs
@@ -1,4 +1,5 @@
-﻿using Nomirun.Sdk.Abstractions.Interfaces;
+﻿using Microsoft.Extensions.Configuration;
+using Nomirun.Sdk.Abstractions.Interfaces;
 using SolarPlatformCommon.Requests;
 using SolarPlatformCommon.Responses;
 
@@ -6,6 +7,13 @@ namespace Accounts.Infrastructure.Handlers
 {
     public class GetAccountDataHandler: IRequestHandler<GetAccountDataRequest, AccountDataResponse>
     {
+        private readonly IConfiguration _configuration;
+
+        public GetAccountDataHandler(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
         public Task<AccountDataResponse> Handle(GetAccountDataRequest request, CancellationToken cancellationToken)
         {
             return Task.FromResult(new AccountDataResponse
